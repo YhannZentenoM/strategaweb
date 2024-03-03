e30c21ebffc4eb1550e4a9faca5fd9fd6edb23dc
José Zenteno <jose.zm.10@gmail.com>
2024-03-03 01:24:36 -0500

    Adicion pagina info equipo, begore bg navbar

---
+++ src/content/config.ts
@@ -0,0 +1,34 @@
+import { defineCollection, z } from 'astro:content';
+
+const servicesCollection = defineCollection({
+    type: 'content', // v2.5.0 y posteriores
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string(),
+    }),
+});
+
+const equipoCollection = defineCollection({
+  type: 'content', // v2.5.0 y posteriores
+  schema: z.object({
+      name: z.string(),
+      social: z.object({
+          instagram: z.string(),
+          linkedin: z.string(),
+          facebook: z.string(),
+      }),
+      image: z.string(),
+      job: z.string(),
+      skills: z.array(z.object({
+          name: z.string(),
+          porcent: z.number(),
+          description: z.string()
+      })),
+  }),
+});
+
+export const collections = {
+  'servicios': servicesCollection,
+  'equipo': equipoCollection,
+};
