47c5e8c88d9da499c88e4ac6157d9a2c0bcc9b6b
Pool <pool.zm@gmail.com>
2024-07-05 14:53:45 -0500

    Eliminar archivos de imágenes de banners y corregir errores de ortografía en títulos de páginas.

--- src/functions/data.ts
+++ src/functions/data.ts
@@ -3,3 +3,9 @@ export async function getData() {
     const data = await response.json();
     return data;
 }
+
+export async function getClients() {
+    const response = await fetch('https://strategaperu.com/php/clientes.json');
+    const data = await response.json();
+    return data;
+}
