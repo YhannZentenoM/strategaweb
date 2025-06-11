7df2dc51f798bc6764c6bce0ec992d860aa87962
Pool <pool.zm@gmail.com>
2025-06-11 12:54:14 -0500

    Actualizacion

--- src/functions/data.ts
+++ src/functions/data.ts
@@ -1,11 +1,11 @@
 export async function getData() {
-    const response = await fetch('https://strategaperu.com/info.json');
-    const data = await response.json();
-    return data;
+  const response = await fetch('https://strategaperu.com/php/info.json');
+  const data = await response.json();
+  return data;
 }
 
 export async function getClients() {
-    const response = await fetch('https://strategaperu.com/php/clientes.json');
-    const data = await response.json();
-    return data;
+  const response = await fetch('https://strategaperu.com/php/clientes.json');
+  const data = await response.json();
+  return data;
 }
